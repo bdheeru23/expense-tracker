b4bf05955ef67a4c22666e61d5d2503eb9d2714c
System Administrator <root@Dheerajs-MacBook-Air.local>
2020-01-18 20:05:36 +0530

    added public folder and init validation on login

--- expense-tracker-ui/src/Components/authentication/Login.js
+++ expense-tracker-ui/src/Components/authentication/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container,Input,Button, FormGroup, Form,InputGroup,InputGroupAddon,Navbar,NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
+import { Container,Input,Button, FormGroup, Form,InputGroup,InputGroupAddon,Navbar,NavbarBrand, Nav, NavItem, NavLink, FormFeedback, FormText} from 'reactstrap';
 import axios from 'axios';
 import {withRouter} from 'react-router-dom';
 
@@ -37,19 +37,28 @@ class Login extends Component {
             "username" : this.state.username,
             "password" : this.state.password
         }
-        axios.post("/authenticate",payload,
-        {
-            headers:{
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(res => {
-            if(res.status ==  200){
-                sessionStorage.setItem("authToken",res.data.token);
-                this.props.history.push("/home")
-            }
-        })
-        .catch(console.log("Error generating auth token"))
+        if(this.validateCredentials){
+            axios.post("/authenticate",payload,
+            {
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => {
+                if(res.status ==  200){
+                    sessionStorage.setItem("authToken",res.data.token);
+                    this.props.history.push("/home")
+                }
+            })
+            .catch(console.log("Error generating auth token"))
+        }
+    }
+
+    validateCredentials(){
+        if(this.state.username && this.state.password){
+            return true;
+        }
+        return false;
     }
 
     render() { 
@@ -68,14 +77,16 @@ class Login extends Component {
                     <Form onSubmit={this.handleSubmit}>
                         <InputGroup className="input-group-sm">
                             <InputGroupAddon addonType="prepend">Username</InputGroupAddon>
-                            <Input type="text" name="username" id="username" placeholder="Username" 
-                                onChange={this.handleUsernameChange} />
+                            <Input type="text" name="username" id="username"  
+                                onChange={this.handleUsernameChange} invalid/>
+                            <FormFeedback invalid>Please input a valid username</FormFeedback>
                         </InputGroup>
                         <br/>
                         <InputGroup className="input-group-sm">
                             <InputGroupAddon addonType="prepend">Password</InputGroupAddon>
-                            <Input type="password" name="password" id="password" placeholder="Password" 
-                                onChange={this.handlePasswordChange} />
+                            <Input type="password" name="password" id="password"  
+                                onChange={this.handlePasswordChange} invalid/>
+                            <FormFeedback invalid>Please input a valid password</FormFeedback>    
                         </InputGroup>
                         <br/>
                         <FormGroup>
